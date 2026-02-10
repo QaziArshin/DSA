be9df666b1ef5a154062e5f349de18bc0e1d0106
QaziArshin <97330974+QaziArshin@users.noreply.github.com>
2026-02-10 23:50:47 +0530

    Update CountElementWithMaximumFrequency.java

--- Arrays/Frequency/CountElementWithMaximumFrequency.java
+++ Arrays/Frequency/CountElementWithMaximumFrequency.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i : nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+            int currfreq = map.get(i);
+        }
+        
+        int maxfrq =0;
+        for(int i : map.values()){
+            maxfrq = Math.max(maxfrq,i);
+        }
+        int count =0;
+        for(int i :map.values()){
+            if(i == maxfrq){
+                count +=i;
+            }
+        }
+        
+        return count;
+    }
+}
